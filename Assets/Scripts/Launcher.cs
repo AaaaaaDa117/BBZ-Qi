1c160932e950f1e69d0db59466bda5da6b2b3624
DirtyWine <769182173@qq.com>
2020-10-11 01:07:55 -0500

    add Lobby and Launcher
    change name of battelScene

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,143 @@
+﻿
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+namespace TurnBase
+{
+    public class Launcher : MonoBehaviourPunCallbacks
+    {
+        #region Private Field
+
+        private string gameVersion = "1";
+
+        private const string playerNamePrefKey = "PlayerName";
+
+        private InputField inputPlayerName;
+        
+
+        #endregion
+
+        #region Public Field
+
+        public GameObject controlPanel;
+        public GameObject loadingPanel;
+
+        public GameObject textConnectFailed;
+
+        public Button buttonConnect;
+
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Debug purpose
+            Debug.Assert(controlPanel != null);
+            Debug.Assert(loadingPanel != null);
+            Debug.Assert(textConnectFailed != null);
+            Debug.Assert(buttonConnect != null);
+
+            //
+            controlPanel.SetActive(true);
+            loadingPanel.SetActive(false);
+            textConnectFailed.SetActive(false);
+
+            // 
+            inputPlayerName = controlPanel.GetComponentInChildren<InputField>();
+            Debug.Assert(inputPlayerName != null);
+
+            string defaultName = string.Empty;
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+            }
+
+            inputPlayerName.text = defaultName;
+
+            PhotonNetwork.NickName = defaultName;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        #region PUN Callbacks
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("Lacuncher: Connected to Master");
+            SceneManager.LoadScene("Lobby");
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            Debug.Log("Disconnect");
+
+            if (controlPanel != null) 
+            {
+                controlPanel.SetActive(true);
+            }
+            if (loadingPanel != null)
+            {
+                loadingPanel.SetActive(false);
+            }
+            if (textConnectFailed != null)
+            {
+                textConnectFailed.SetActive(true);
+            }
+        }
+        
+
+        #endregion
+
+        #region Public Methods
+
+        public void Connect()
+        {
+            Debug.Log("Lacuncher: Try Connecting...");
+            if (!PhotonNetwork.IsConnected)
+            {
+                PlayerPrefs.SetString(playerNamePrefKey, PhotonNetwork.NickName);
+
+                controlPanel.SetActive(false);
+                loadingPanel.SetActive(true);
+
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = this.gameVersion;
+                PhotonNetwork.AutomaticallySyncScene = true;
+            }
+            else
+            {
+                // Todo
+            }
+        }
+
+        public void SetPlayerName(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                buttonConnect.interactable = false;
+                return;
+            }
+
+            buttonConnect.interactable = true;
+            
+            PhotonNetwork.NickName = value;
+
+            //Debug.Log("New name: " + value);
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
+
+        #endregion
+    }
+}
+
---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace TurnBase
+{
+    public class LobbyManager : MonoBehaviourPunCallbacks
+    {
+        #region Private Fields
+
+        private string myNickName;
+        private byte maxPlayersPerRoom = 2;
+
+        #endregion
+
+        #region Public Fields
+        public Transform transformLobbyUI;
+        public Transform transformMatchingUI;
+
+        public Transform targetTrans;
+
+        public Camera mCamera;
+        #endregion
+
+        #region MonoBehaviour
+        // Start is called before the first frame update
+        void Start()
+        {
+            myNickName = PhotonNetwork.NickName;
+
+            mCamera = Camera.main;
+            targetTrans = transformLobbyUI;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Quick and dirty
+            // Need to rework
+            if (!mCamera.transform.position.Equals(targetTrans.position)) 
+            {
+                mCamera.transform.position = Vector3.MoveTowards(mCamera.transform.position, targetTrans.position, 600.0f*Time.deltaTime);
+            }
+
+            if (!mCamera.transform.rotation.Equals(targetTrans.rotation))
+            {
+                mCamera.transform.rotation = Quaternion.RotateTowards(mCamera.transform.rotation, targetTrans.rotation, 600.0f * Time.deltaTime);
+            }
+
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                targetTrans = transformMatchingUI;
+            }
+            if(Input.GetKeyDown(KeyCode.D))
+            {
+                targetTrans = transformLobbyUI;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void QuickMatch()
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        #endregion
+
+        #region PUN Callbacks
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("Lobby: On Join Random Failed");
+            Debug.Log("Lobby: Creating a Room...");
+
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = this.maxPlayersPerRoom });
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("Lobby: On Joined Room()");
+            targetTrans = transformMatchingUI;
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            Debug.Log("Lobby: On Player Entered Room(), Player: " + newPlayer.NickName);
+            if (PhotonNetwork.CurrentRoom.PlayerCount == this.maxPlayersPerRoom)
+            {
+                Debug.Log("Lobby: Opponent found! Loading battle scene");
+
+                if (PhotonNetwork.IsMasterClient)
+                {
+                    PhotonNetwork.LoadLevel("BattleScene");
+                }
+
+            }
+        }
+        #endregion
+    }
+}
+
