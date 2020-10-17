bdd680cf02c7bbc463b5c2e19cf38fb28de9408b
AaaaaaDa117 <AaaaaaDa117@users.noreply.github.com>
2020-10-18 05:36:59 +0800

    Start UI
    new anim

---
+++ Assets/Scripts/UIScripts/ObjPool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjPool : MonoBehaviour
+{
+    Transform tf;
+    Transform activeTf;
+
+    SpriteRenderer thisSprite;
+    SpriteRenderer sprite;
+
+    Color color;
+
+    public float activeTime;
+    public float activeStart;
+
+    float alpha;
+    public float alphaSet;
+    public float alphaMultiplier;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        tf = GameObject.FindGameObjectWithTag("Logo").transform;
+        thisSprite = GetComponent<SpriteRenderer>();
+        sprite = tf.GetComponent<SpriteRenderer>();
+
+        alpha = alphaSet;
+        thisSprite.sprite = sprite.sprite;
+        transform.position = tf.position;
+        transform.localScale = tf.localScale;
+        //transform.rotation = tf.rotation * 0.8f;
+        Quaternion r = Quaternion.Euler(0,0, tf.rotation.z * 0.8f);
+
+        transform.rotation = r;
+
+        activeStart = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alpha *= alphaMultiplier;
+        color = new Color(1, 1, 1, alpha);
+        thisSprite.color = color;
+        if (Time.time >= activeStart + activeTime)
+        {
+            ShadowPool.instance.ReturnPool(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UIScripts/ShadowPool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowPool : MonoBehaviour
+{
+    public static ShadowPool instance;
+
+    public GameObject shadowPrefab;
+
+    public int shadowCount;
+    Queue<GameObject> availableObj = new Queue<GameObject>();
+    private void Awake()
+    {
+        instance = this;
+        FillPool();
+    }
+
+    public void FillPool()
+    {
+        for (int i = 0; i < shadowCount; i++)
+        {
+            var newShadow = Instantiate(shadowPrefab);
+            newShadow.transform.SetParent(transform);
+            ReturnPool(newShadow);
+        }
+    }
+    public void ReturnPool(GameObject gameObj)
+    {
+        gameObj.SetActive(false);
+        availableObj.Enqueue(gameObj);
+    }
+    public GameObject GetFromPool()
+    {
+        if (availableObj.Count == 0)
+        {
+            FillPool();
+        }
+        var outShadow = availableObj.Dequeue();
+        outShadow.SetActive(true);
+        return outShadow;
+    }
+    private void Update()
+    {
+        GetFromPool();
+    }
+}
